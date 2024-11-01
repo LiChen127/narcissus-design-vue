8741a80f799d2dcee26e16b748dbacc4e9a137e7
李辰 <2095055676@qq.com>
2024-11-01 10:02:39 +0800

    ci: 尝试配置npmrc解决工作区的包没有链接的问题

--- packages/button/src/button.vue
+++ packages/button/src/button.vue
@@ -1,11 +1,17 @@
-<script setup lang='ts'>
+<script lang="ts" setup name="narcissus-button">
+import { defineProps } from 'vue';
+import { buttonProps } from './types';
 
+defineProps(buttonProps);
 </script>
 
 <template>
-  <div />
+  <div>
+    narcissus-button
+  </div>
 </template>
 
-<style lang='scss' scoped>
-
+<style scoped lang="scss">
+  .narcissus-button {
+  }
 </style>
--- packages/utils/src/test-log.ts
+++ packages/utils/src/test-log.ts
@@ -1,5 +1,4 @@
 // 测试使用的工具函数
 export const testLog = (str: string) => {
   console.log('test log', str);
-}
-
+};

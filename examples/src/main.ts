f4382b6985ff3bbe137beeabf0bc9b25f3cf7966
李辰 <2095055676@qq.com>
2024-11-01 09:03:58 +0800

    ci: 清除elementplush依赖

--- examples/src/main.ts
+++ examples/src/main.ts
@@ -1,8 +1,4 @@
-// const env = import.meta.env
-// console.log(env)
 import { createApp } from 'vue'
-import './style.css'
-import 'element-plus/dist/index.css';
 import NarcissusDesignVue from '@narcissus-design-vue/narcissus-design-vue';
 import App from './App.vue'
 const app = createApp(App);

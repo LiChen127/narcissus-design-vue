75c616889368f33f4cbb9b5c5fec64f3f1036f76
李辰 <2095055676@qq.com>
2024-11-01 08:48:31 +0800

    refactor: 去除测试时使用的elementplus依赖

--- docs/.vitepress/theme/index.ts
+++ docs/.vitepress/theme/index.ts
@@ -1,16 +1,13 @@
-import DefaultTheme from "vitepress/theme";
+import DefaultTheme from 'vitepress/theme';
 import { AntDesignContainer } from '@vitepress-demo-preview/component';
-import '@vitepress-demo-preview/component/dist/style.css'
-import ElementPlus from 'element-plus';
-import 'element-plus/dist/index.css';
+import '@vitepress-demo-preview/component/dist/style.css';
 import NarcissusUI from '@narcissus-design-vue/narcissus-design-vue';
-import { EnhanceAppContext } from 'vitepress'
+import { EnhanceAppContext } from 'vitepress';
 
 export default {
   ...DefaultTheme,
   enhanceApp(ctx: EnhanceAppContext) {
-    ctx.app.use(ElementPlus)
-    ctx.app.use(NarcissusUI)
-    ctx.app.component('demo-preview', AntDesignContainer)
-  }
-}
+    ctx.app.use(NarcissusUI);
+    ctx.app.component('demo-preview', AntDesignContainer);
+  },
+};
--- examples/src/main.ts
+++ examples/src/main.ts
@@ -2,12 +2,10 @@
 // console.log(env)
 import { createApp } from 'vue'
 import './style.css'
-import ElementPlus from 'element-plus';
 import 'element-plus/dist/index.css';
 import NarcissusDesignVue from '@narcissus-design-vue/narcissus-design-vue';
 import App from './App.vue'
 const app = createApp(App);
 console.log(NarcissusDesignVue);
-app.use(ElementPlus);
 app.use(NarcissusDesignVue);
 app.mount('#app');

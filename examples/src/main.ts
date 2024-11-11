0947ac9280ba454e42d9c8c96a0dc6e95d8a3f37
李辰 <2095055676@qq.com>
2024-11-11 21:59:53 +0800

    ci: 调整组件库测试文件结构，引入路由

--- examples/src/main.ts
+++ examples/src/main.ts
@@ -1,7 +1,6 @@
 import { createApp } from 'vue'
 import NarcissusDesignVue from '@narcissus-design-vue/narcissus-design-vue';
-// import { router } from './router';
-import router from '@/router/index';
+import router from '../src/router/index';
 import App from './App.vue'
 const app = createApp(App);
 app.use(router);
